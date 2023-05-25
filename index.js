ea8ca798fc6bbaed83aeec588408fc2113646141
Artur <artur97s@yandex.ru>
2023-05-25 20:07:55 +0300

    feat: add sound

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
-const btn4 = document.querySelector(".btn4");
-const btn5 = document.querySelector(".btn5");
+const sound1 = new Audio("./sounds/1.wav")
+const sound2 = new Audio("./sounds/2.wav")
+const sound3 = new Audio("./sounds/3.wav")
+const sound4 = new Audio("./sounds/4.wav")
+const sound5 = new Audio("./sounds/5.wav")
+
+let arr = [sound1, sound2, sound3, sound4, sound5]
+
+const btnList = document.querySelector(".btn-list");
+
+btnList.addEventListener("mousedown", (e) => {
+  console.log(e.target.id)
+  let num = e.target.id;
+  arr[e.target.id].play()
+})
